cb035ea3d454afa6f03a158e7d6af73bc3d2dc16
kazupong <kzpng2@gmail.com>
2018-05-31 10:30:21 -0700

    Initial Commit

---
+++ demo_Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo_Camera
+//
+//  Created by Kazuyuki Nakatsu on 5/31/18.
+//  Copyright © 2018 Kazuyuki Nakatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
