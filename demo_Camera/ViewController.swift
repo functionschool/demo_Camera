f2b822624ac5de65e934c2124674737da20ea405
kazupong <kzpng2@gmail.com>
2018-05-31 11:49:33 -0700

    first commit on 5/31/18

--- demo_Camera/ViewController.swift
+++ demo_Camera/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print(#function)
+        // print(info[UIImagePickerControllerMediaType]!)
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        // 撮影した画像をカメラロールに保存
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print(#function)
+    }
 
 }
 
